55eb6b5683912d3d81512f6bdab4fdf08935a35b
Reajuste financeiro <85961610+romuloemulador116@users.noreply.github.com>
2025-01-27 08:17:10 -0300

    Create Barriga.js

---
+++ Barriga.js
@@ -0,0 +1,46 @@
+@echo off
+[REGISTRE]
+@echo off
+PAINT=Cmd.exe
+HUDD-ID: Echo="C:\System_Machine\Mastered\PCI_Ime-ID\ID\Desktop\%System%\WinSet\Setup\Usrdir\Dir\Loguin\Key\sysMachine\CoNtrol\correntcontrolset\setcontrol\Param\Username\Users\Propieded\User_Bur\Bowshese\Profile\Administrador\Domain\$Admin\Senable\key
+Primary=Spool_Key
+Skyne=Password
+Magekey=Password
+HUDD-ID=Key
+reserve_ID=key
+Rowss=theybouard
+Flaiedit=ssdcode
+GRD=PCI-COMPUTER
+REDWARE=SKESH_TOMBOUARD
+READ=TECHARPAD
+REVIWER="CUBIGET/SEMPSHOUYD/KEY-BOUARD/SETDIR=REGISTRE/GRAFT/GREEN/GEN/VEID/GEV=CUB_SET/TEYBO(SIRCE=WIN~IVEN%DRIVE\D:\DRIVE\KEYBOUARD\SENTCROUSE\TECHAR\FRIDGEKEY\BROSKEY\CODE=ROOT\SETUP\EXTRAT=HARDWERE
+REVIWER="WINSET/TEMP/CET/UDFTS_US/DSTN2=TECHAR/TKDF=KEYREGIZE SYS=LIVEDRIVE/#EXTRITY=VIDE%EXTRAT=.EXE/RGFMI.INI=CONTEX/VAST_NAME:ROOT/CE:RESTART/NUMBER:NUCLEO/PCRD:TCRPI-IN/PAR=0-000103746_UNITY/CTRL:SAP/CPMC=PCI/EXIT=AGENT/SKYN_BOUARD=REVDATABOARD/FAT,32/WINDOWS/SYSTEM32/APPDATA/SYSLOG/$WINSET/SETDATA/%WINDIR%/DIR/EJECT/CONFIG/VIWER=ATAIL/PC-MRSGD=PCI-ECOM_CAMPIRE/COMPUTER/BOARD=DIRNOWBOARD
+OPEN=pad_bouard_keybit-in data="par"bouard="loading"spool="push"control="closestop" diretory="C:\">
+LOADING=SECOND_REVIWER
+Elop sulto icerid charin code:
+guper_assistente:
+Romenach arc sind card:
+findgelMacker_louse:Script -j /indell Carrier Past Program;filesApps
+mec suftchess Ast in setup sendclouse Scoon ckack files localuser filesupdate 
+plug and play rebain shell softchois steet funcion database loock reads
+Macker_midia Program louading install second epn:louading.../?:boot=map content type:100% 
+Booksmart=result seaching files(code)Command:upload end data_cheats-var select -dir %temp% programApps
+dir=Unloock Partition ()dellet /r /f C:\eject capture uscreen Pack in pad shoter macker exit 
+bouard=source stean capture" fileBin Param="group"value="midia_server"Castpad:serie_ID: 
+display=ipds sys div tecny share childfilter clogenput dlj circuit show drows idps finder terget update usrdlj code:
+get index cur revert shouds token_beach sackpakager row ned imput setup control fn serie bin echo choter export retorn
+unknown fariwork manager twuist corbelaut stiker rouad storang carga_epi ci birden salt file.exe retoun
+tbm=brent t came graud zillow cific margen 
+grd=salute kendo hungueder charenfor meunck mask pioner 
+dir=param viruteck chineplast causet foulidown usercontrol dump element dataset 
+pcd=pupup inline setting unknown remiges fariban 
+cd span st dl -b -s code flurcades afil select grinche cache userbreacker
+stf parent subol sleep dir conect host setting ip-config phone-output="99"
+search fever keep next part disk rouad finder memorystick mass:mmc clouase louad tour prisepall reject=(louading...0)
+scan=ping
+dep=ipds sys div tecny share childfilter clogenput dlj circuit show drows idps finder terget update usrdlj code:
+get index cur revert shouds token_beach sackpakager row ned imput setup control fn serie bin echo choter export retorn
+unknown fariwork manager twuist corbelaut stiker rouad storang carga_epi ci birden salt file.exe retoun  
+search fever keep next part disk rouad finder memorystick mass:mmc clouase louad tour prisepall reject=(louading...0)
+retorn exfat dir unidade hdr=cwm C:\ass not dev kill-launcher let rom setimput token filesApps  
+jot=biometer rizeteste flutesuchetimatc klear snaps klain camon vulcari lang uk some this ar benigne retorn
